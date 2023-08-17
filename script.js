9f8c8c7e3d8c7c77b74d51502636129bf878d05d
o77777o <136020078+o77777o@users.noreply.github.com>
2023-08-17 21:24:31 +0300

    function

---
+++ script.js
@@ -0,0 +1,20 @@
+function plus(a, b) {
+  return a + b;
+}
+
+function minus(a, b) {
+  return a - b;
+}
+
+function multy(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+a = 17
+b = 34
+
+alert (plus())
