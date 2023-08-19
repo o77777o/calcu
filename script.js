c5f29abca1700bcc21aa5c5ed78dafeed2ae39e4
o77777o <136020078+o77777o@users.noreply.github.com>
2023-08-19 17:20:08 +0300

    done

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function divide(a, b) {
   return a / b;
 }
 
-function calculate(a, b, operator) {
+// let operator = ["+", "-", "*", "/"];
+
+function operation(a, b, operator) {
   if (operator === "+") {
     return plus(a, b);
   }
@@ -27,4 +29,18 @@ function calculate(a, b, operator) {
   if (operator === "/") {
     return divide(a, b);
   }
+}
+
+function calculate() {
+  let InputNum1 = document.getElementById("a");
+  let InputOperator = document.getElementById("operator");
+  let InputNum2 = document.getElementById("b");
+  //  --------
+  const num1 = InputNum1.value;
+  const operator = InputOperator.value;
+  const num2 = InputNum2.value;
+
+  let OutputResult = document.getElementById("result");
+  const result = operation(+num1, +num2, operator);
+  OutputResult.value = result;
 }
