2c2ae3348bc02bf00e1544d31399bf2cd33045d9
Mikel <136020078+o77777o@users.noreply.github.com>
2023-09-05 23:06:36 +0300

    in progress 1

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-let a = ''
-let b = ''
-let operator = ''
+let a = ""; //память 1 числа
+let b = ""; //память 2 числа
+let operator = ""; //знак
+let stop = false;
+
+//разделяем цифры от знаков
+const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ","];
+const action = ["+", "-", "/", "X"];
+//Экран
+const screen = document.querySelector(".screen");
+
+function AC() {
+  a = "";
+  b = "";
+  operator = "";
+  stop = false;
+  screen.textContent = 0;
+  console.log(a, b, operator, stop);
+}
+
+document.querySelector(".ac").onclick = AC;
+
+document.querySelector(".buttons").onclick = (event) => {
+  if (event.target.classList.contains('ac')) return;
+  if (!event.target.classList.contains("btn")) return;
+
+  screen.textContent = "";
+  const key = event.target.textContent;
+
+  if (numbers.includes(key)) {
+    a += key;
+    console.log(a, b, operator);
+  }
+
+  screen.textContent = a
+};
