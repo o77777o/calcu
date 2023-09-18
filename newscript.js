418d87f615a5bab7161fd31b569ad215ec30225f
o77777o <136020078+o77777o@users.noreply.github.com>
2023-09-18 09:58:19 +0300

    Update newscript.js

--- newscript.js
+++ newscript.js
@@ -3,12 +3,42 @@ let b = ""; //память 2 числа
 let operator = ""; //знак
 
 //разделяем цифры от знаков
-const numbers = document.querySelectorAll(".numbers")
-const action = document.querySelectorAll(".operators")
+const numbers = document.querySelectorAll(".numbers");
+const action = document.querySelectorAll(".operators");
+const screen = document.querySelector(".screen");
 
-console.log(numbers.length)
-console.log(action.length)
+console.log(numbers.length);
+console.log(action.length);
 
-for(i=0; i<12; i++) {
+function AC() {
+  a = "";
+  b = "";
+  operator = "";
+  screen.textContent = "0";
+  console.log("Поле очищено");
+}
 
+document.querySelector(".ac").onclick = AC;
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", function () {
+    if (b === "" && operator === "") {
+      a += this.innerText;
+      screen.textContent = a;
+    }
+    if (operator !== "") {
+      b += this.innerText;
+      screen.textContent = b;
+    }
+
+    console.log(a, b, operator);
+  });
+}
+
+for (let i = 0; i < action.length; i++) {
+  action[i].addEventListener("click", function () {
+    operator = this.innerText;
+    screen.textContent = operator;
+    console.log(a, b, operator);
+  });
 }
